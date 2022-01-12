8ad3d76e283b406a96be2f3e068f3dce5b23b6b2
Elliot <elliotb76@gmail.com>
2022-01-12 11:10:16 -0500

    removed "Better not tell you now.", it's a dumb answer

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ magic8Responses1 = [
     "It is certain.", "It is decidedly so.", "Without a doubt.",
     "Yes – definitely.", "You may rely on it.", "As I see it, yes.",
     "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
-    "Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
+    "Reply hazy, try again.", "Ask again later.",
     "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.",
     "My reply is no.", "My sources say no.", "Outlook not so good.",
     "Very doubtful."
 ]
+#removed "Better not tell you now.", it's a dumb answer
 
 uWuWords = [
     "uwu", "owo", "daddy", "moist", "daddi", "dad"
